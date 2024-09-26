f816093b47bbfda0933ce7cf671e822e751f5d6f
Yuri Alexandre Pires de Carvalho <yuri.apcarvalho@gmail.com>
2024-09-26 00:10:14 -0400

    ajuste cors

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,12 @@ app.set('queues', {
 const bodyparser = require('body-parser');
 app.use(bodyParser.json({ limit: '10mb' }));
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(Sentry.Handlers.requestHandler());
