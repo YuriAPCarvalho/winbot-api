c60fed814f452d892dcf0fbf4f93e70abf74b944
alanarg <alanarguelho2@gmail.com>
2024-10-06 22:52:27 -0400

    Shellscript

---
+++ deploy_api.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Parar o container
+docker stop hml-winbot-api
+
+# Remover o container
+docker rm hml-winbot-api
+
+# Remover a imagem
+docker rmi hml-winbot-api
+
+# Atualizar o repositÃ³rio
+git pull
+
+# Construir a nova imagem
+docker build -t image/hml-winbot-api .
+
+# Executar o novo container
+docker run -d --env-file .env -p 4001:4001 -e PORT=4001 --name hml-winbot-api image/hml-winbot-api
